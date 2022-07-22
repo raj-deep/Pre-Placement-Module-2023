f50035740d6d3a8953baffdc44a6e916d06654e6
raj-deep <deepraj181200@gmail.com>
2022-07-22 22:47:07 +0530

    commit

---
+++ DS II/Day 1/MajorElements.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int major = nums[0];
+        int count = 1;
+        for(int i=1; i<nums.length; i++){
+            if(major == nums[i])
+                count++;
+            else
+                count--;
+            if(count == 0){
+                major = nums[i];
+                count = 1;
+            }
+        }
+        
+        return major;
+    }
+}
